98a493b0344e2e7823554d8d144aff154826b437
junioranheu <junioranheu@gmail.com>
2024-10-27 00:00:18 -0300

    initial structuring

---
+++ Auth.API/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+
+        [HttpGet]
+        public DateTime Get()
+        {
+            return DateTime.Now;
+        }
+    }
+}
