1e5b208f2aa1853d4253a105dffa4a4cd1373fd8
junioranheu <junioranheu@gmail.com>
2024-10-28 23:46:17 -0300

    refactor TokenRefreshMiddleware

--- Auth.API/Middlewares/TokenRefreshMiddleware.cs
+++ Auth.API/Middlewares/TokenRefreshMiddleware.cs
@@ -4,14 +4,20 @@ using System.IdentityModel.Tokens.Jwt;
 
 namespace Auth.API.Middlewares;
 
-public sealed class TokenRefreshMiddleware(RequestDelegate next, IJwtTokenGenerator jwtTokenGenerator, ICreateRefreshToken createRefreshToken)
+public sealed class TokenRefreshMiddleware(RequestDelegate next, IJwtTokenGenerator jwtTokenGenerator, IServiceScopeFactory scopeFactory)
 {
     private readonly RequestDelegate _next = next;
     private readonly IJwtTokenGenerator _jwtTokenGenerator = jwtTokenGenerator;
-    private readonly ICreateRefreshToken _createRefreshToken = createRefreshToken;
+    private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
 
     public async Task InvokeAsync(HttpContext context)
     {
+        if (!context.Request.Headers.ContainsKey("Authorization"))
+        {
+            await _next(context);
+            return;
+        }
+
         string token = context.Request.Headers.Authorization.ToString().Replace("Bearer ", "");
 
         if (!string.IsNullOrEmpty(token))
@@ -21,7 +27,11 @@ public sealed class TokenRefreshMiddleware(RequestDelegate next, IJwtTokenGenera
             if (_jwtTokenGenerator.IsTokenExpiringSoon(jwtToken))
             {
                 Guid userId = Guid.NewGuid(); // TO DO: OBTER O USUÁRIO!!!!!!!!!!!
-                (string newJwtToken, string newRefreshToken) = await _createRefreshToken.RefreshToken(userId);
+
+                using IServiceScope scope = _scopeFactory.CreateScope();
+                ICreateRefreshToken createRefreshToken = scope.ServiceProvider.GetRequiredService<ICreateRefreshToken>();
+
+                (string newJwtToken, string newRefreshToken) = await createRefreshToken.RefreshToken(userId);
 
                 // Update context;
                 context.Response.Headers.Authorization = "Bearer " + newJwtToken;
